c503aeb9c709968bf4012c0c7f3b54182f6902cd
Coleen Dian Fernandez <coleendian.fernandez@ust.edu.ph>
2026-08-31 16:54:24 +0800

    second commit

--- member5-task4-functions-and-parameters.py
+++ member5-task4-functions-and-parameters.py
@@ -1,1 +1,7 @@
-print("Hello, population!")
+def validate_input(force, area, original_lenght, change_in_lenght):
+    if force < 0:
+        raise ValueError("Force must be a positive value.")
+    if area <= 0:
+        raise ValueError("Area must be a positive value.")
+    if original_lenght <= 0:
+        raise ValueError("Original lenght must be a positive value.")
