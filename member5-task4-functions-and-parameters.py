054a8d7a4654d2b30caeb44ad33bd5165fa3f5e1
Coleen Dian Fernandez <coleendian.fernandez@ust.edu.ph>
2026-08-31 16:59:41 +0800

    change in lenght

--- member5-task4-functions-and-parameters.py
+++ member5-task4-functions-and-parameters.py
@@ -5,3 +5,5 @@ def validate_input(force, area, original_lenght, change_in_lenght):
         raise ValueError("Area must be a positive value.")
     if original_lenght <= 0:
         raise ValueError("Original lenght must be a positive value.")
+    if change_in_lenght < 0:
+        raise ValueError("Change in lenght must be a positive value.")
