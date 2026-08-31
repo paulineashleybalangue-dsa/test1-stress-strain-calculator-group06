fe8d33dad7345804037d8147b49b6b409ab7a9e0
Mary Belle Cubol <bellecubol16@gmail.com>
2026-08-31 17:49:04 +0800

    Validate area change

--- member2-task2-control-structures.py
+++ member2-task2-control-structures.py
@@ -97,15 +97,20 @@ def main():
                     continue
                 break
             except ValueError:
-                print("Please enter a valid numeric value for Force!")
-                
+                print("Please enter a valid numeric value for Force!(or 'q' to quit)!")
+
         if abort_calculation:
             print("\n[Calculation Canceled] Returning to main menu.")
             continue
 
+        #Validating Area
         while True:
+            raw_val = input("Enter cross-sectional area (m^2): ").strip()
+            if raw_val.lower() in ['q', 'quit']:
+                abort_calculation = True
+                break
             try:
-                area = float(input("Enter cross-sectional area (m^2): "))
+                area = float(raw_val)
                 if area <= 0:
                     print("Cross-sectional area must be greater than zero (prevents division error)!")
                     continue
@@ -113,6 +118,11 @@ def main():
             except ValueError:
                 print("Please enter a valid numeric value for Area!")
 
+        if abort_calculation:
+            print("\n[Calculation Canceled] Returning to main menu.")
+            continue
+
+        #Validate Original Length
         while True:
             try:
                 original_length = float(input("Enter original length (m): "))
