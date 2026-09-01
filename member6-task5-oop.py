788a3f270f63f109b61616a868039b6692ed71ba
Meg <meghanisabelle.espiritu@ust.edu.com>
2026-09-01 13:25:18 +0800

    implement stress capability check

--- member6-task5-oop.py
+++ member6-task5-oop.py
@@ -4,8 +4,8 @@ from typing import List
 @dataclass
 class MaterialProperties:
     density: float  # kg/m
-    yield_strength: float  
-    typical_youngs_modulus: float 
+    yield_strength: float  # MPa
+    typical_youngs_modulus: float # GPa
 
     def __post_init__(self):
         if self.density <= 0 or self.yield_strength <= 0 or self.typical_youngs_modulus <= 0:
@@ -14,3 +14,6 @@ class Material:
     def __init__(self, name: str, properties: MaterialProperties):
         self.name = name
         self.properties = properties
+    def can_withstand_stress(self, stress_mpa: float) -> bool:
+        """Check if the material can withstand the given stress (in MPa)."""
+        return stress_mpa < self.properties.yield_strength
