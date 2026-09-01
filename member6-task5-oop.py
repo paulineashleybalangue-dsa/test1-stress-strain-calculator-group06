fee43e2a0d5e4862d3bc85e9317efe35c5b4399c
Meg <meghanisabelle.espiritu@ust.edu.com>
2026-09-01 13:16:30 +0800

    add validation for material properties

--- member6-task5-oop.py
+++ member6-task5-oop.py
@@ -1,2 +1,8 @@
 from dataclasses import dataclass
 from typing import List
+
+@dataclass
+class MaterialProperties:
+    density: float  # kg/m
+    yield_strength: float  
+    typical_youngs_modulus: float 
