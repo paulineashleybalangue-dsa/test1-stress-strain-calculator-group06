e36c55054c66fd0475eb81f8ecb3e5812397c167
Meg <meghanisabelle.espiritu@ust.edu.com>
2026-09-01 13:22:25 +0800

    added onto MaterialProperties class, validation and raise value error

--- member6-task5-oop.py
+++ member6-task5-oop.py
@@ -6,3 +6,7 @@ class MaterialProperties:
     density: float  # kg/m
     yield_strength: float  
     typical_youngs_modulus: float 
+
+    def __post_init__(self):
+        if self.density <= 0 or self.yield_strength <= 0 or self.typical_youngs_modulus <= 0:
+            raise ValueError("All material properties must be positive values.")
