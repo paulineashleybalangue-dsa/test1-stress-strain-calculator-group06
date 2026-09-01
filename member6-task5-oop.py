c6d94816b43e629fe748fce2492d1304401a6940
Meg <meghanisabelle.espiritu@ust.edu.com>
2026-09-01 13:23:50 +0800

    create base material class

--- member6-task5-oop.py
+++ member6-task5-oop.py
@@ -10,3 +10,7 @@ class MaterialProperties:
     def __post_init__(self):
         if self.density <= 0 or self.yield_strength <= 0 or self.typical_youngs_modulus <= 0:
             raise ValueError("All material properties must be positive values.")
+class Material:
+    def __init__(self, name: str, properties: MaterialProperties):
+        self.name = name
+        self.properties = properties
